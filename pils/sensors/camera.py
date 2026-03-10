51ff246505b4c4f3f432276a245deeb0ddb8b54a
Francisco Granda <francisco.granda@starspectech.com>
2026-03-10 10:11:07 -0400

    fix: Updated correct imports.

--- pils/sensors/camera.py
+++ pils/sensors/camera.py
@@ -302,8 +302,6 @@ class Camera:
         >>> df['qw'].mean()  # Quaternion w component
         """
         try:
-            import telemetry_parser  # lazy: only needed for Sony camera parsing
-
             parser = telemetry_parser.Parser(path)  # type: ignore
             imu_data = parser.normalized_imu()
         except Exception as e:
