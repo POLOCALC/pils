fe7966837d298b7206252089ab95390f99bf6bf0
Gabriele Coppi <gabriele.coppi@unimib.it>
2026-03-16 14:50:52 +0100

    Enhance docstring to include ENU coordinates
    Updated return type description to include ENU coordinates.

--- pils/analyze/azel.py
+++ pils/analyze/azel.py
@@ -235,8 +235,8 @@ class AZELAnalysis:
         Returns
         -------
         AZELVersion | None
-            AZELVersion with computed azimuth, elevation, slant range data and metadata,
-            or None if no valid RTK data available after filtering.
+            AZELVersion with computed azimuth, elevation, slant range, ENU coordinates data 
+            and metadata, or None if no valid RTK data available after filtering.
 
         Raises
         ------
